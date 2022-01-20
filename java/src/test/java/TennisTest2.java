586f23a4aa44acb7b6aaf5f347a74c8154fbebd9
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-01-20 12:37:59 +0100

    Divide tests

--- java/src/test/java/TennisTest2.java
+++ java/src/test/java/TennisTest2.java
@@ -9,7 +9,7 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 @RunWith(Parameterized.class)
-public class TennisTest {
+public class TennisTest2 {
 
     private int player1Score;
     private int player2Score;
@@ -17,14 +17,14 @@ public class TennisTest {
     private String player2Name;
     private String expectedScore;
 
-    public TennisTest(int player1Score, int player2Score, String player1Name, String player2Name, String expectedScore) {
+    public TennisTest2(int player1Score, int player2Score, String player1Name, String player2Name, String expectedScore) {
         this.player1Score = player1Score;
         this.player2Score = player2Score;
         this.expectedScore = expectedScore;
         this.player1Name = player1Name;
         this.player2Name = player2Name;
     }
-    
+
     @Parameters(name = "{0}:{1}:{3}:{4} = {2}")
     public static Collection<Object[]> getAllScores() {
         return Arrays.asList(new Object[][] {
@@ -33,7 +33,7 @@ public class TennisTest {
                 { 2, 2,  "player1", "player2",  "Thirty-All"},
                 { 3, 3,  "player1", "player2",  "Deuce"},
                 { 4, 4,  "player1", "player2",  "Deuce"},
-                
+
                 { 1, 0,  "player1", "player2",  "Fifteen-Love"},
                 { 0, 1,  "player1", "player2",  "Love-Fifteen"},
                 { 2, 0,  "player1", "player2",  "Thirty-Love"},
@@ -42,7 +42,7 @@ public class TennisTest {
                 { 0, 3,  "player1", "player2",  "Love-Forty"},
                 { 4, 0,  "player1", "player2",  "Win for player1"},
                 { 0, 4,  "player1", "player2",  "Win for player2"},
-                
+
                 { 2, 1,  "player1", "player2",  "Thirty-Fifteen"},
                 { 1, 2,  "player1", "player2",  "Fifteen-Thirty"},
                 { 3, 1,  "player1", "player2",  "Forty-Fifteen"},
@@ -54,7 +54,7 @@ public class TennisTest {
                 { 2, 3,  "player1", "player2",  "Thirty-Forty"},
                 { 4, 2,  "player1", "player2",  "Win for player1"},
                 { 2, 4,  "player1", "player2",  "Win for player2"},
-                
+
                 { 4, 3,  "player1", "player2",  "Advantage player1"},
                 { 3, 4,  "player1", "player2",  "Advantage player2"},
                 { 5, 4,  "player1", "player2",  "Advantage player1"},
@@ -66,6 +66,8 @@ public class TennisTest {
                 { 4, 6,  "player1", "player2",  "Win for player2"},
                 { 16, 14,  "player1", "player2",  "Win for player1"},
                 { 14, 16,  "player1", "player2",  "Win for player2"},
+                { 14, 16,  "playerA", "playerB",  "Win for playerB"},
+
         });
     }
 
@@ -81,27 +83,10 @@ public class TennisTest {
     }
 
     @Test
-    public void checkAllScoresTennisGame1() {
-        TennisGame1 game = new TennisGame1(this.player1Name, this.player2Name);
-        checkAllScores(game);
-    }
-
-    @Test
     public void checkAllScoresTennisGame2() {
         TennisGame2 game = new TennisGame2(this.player1Name, this.player2Name);
         checkAllScores(game);
     }
 
-    @Test
-    public void checkAllScoresTennisGame3() {
-        TennisGame3 game = new TennisGame3(this.player1Name, this.player2Name);
-        checkAllScores(game);
-    }
-
-    @Test
-    public void checkAllScoresTennisGame4() {
-        TennisGame game = new TennisGame4(this.player1Name, this.player2Name);
-        checkAllScores(game);
-    }
 
 }
