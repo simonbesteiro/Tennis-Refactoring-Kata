835f618cb6a9ba697833fe8dd645e25c6367b408
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-01-20 12:28:11 +0100

    Scaffolding

--- java/src/test/java/TennisTest.java
+++ java/src/test/java/TennisTest.java
@@ -13,55 +13,59 @@ public class TennisTest {
 
     private int player1Score;
     private int player2Score;
+    private String player1Name;
+    private String player2Name;
     private String expectedScore;
 
-    public TennisTest(int player1Score, int player2Score, String expectedScore) {
+    public TennisTest(int player1Score, int player2Score, String player1Name, String player2Name, String expectedScore) {
         this.player1Score = player1Score;
         this.player2Score = player2Score;
         this.expectedScore = expectedScore;
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
     }
     
-    @Parameters(name = "{0}:{1} = {2}")
+    @Parameters(name = "{0}:{1}:{3}:{4} = {2}")
     public static Collection<Object[]> getAllScores() {
         return Arrays.asList(new Object[][] {
-                { 0, 0, "Love-All" },
-                { 1, 1, "Fifteen-All" },
-                { 2, 2, "Thirty-All"},
-                { 3, 3, "Deuce"},
-                { 4, 4, "Deuce"},
+                { 0, 0,  "player1", "player2",  "Love-All" },
+                { 1, 1,  "player1", "player2",  "Fifteen-All" },
+                { 2, 2,  "player1", "player2",  "Thirty-All"},
+                { 3, 3,  "player1", "player2",  "Deuce"},
+                { 4, 4,  "player1", "player2",  "Deuce"},
                 
-                { 1, 0, "Fifteen-Love"},
-                { 0, 1, "Love-Fifteen"},
-                { 2, 0, "Thirty-Love"},
-                { 0, 2, "Love-Thirty"},
-                { 3, 0, "Forty-Love"},
-                { 0, 3, "Love-Forty"},
-                { 4, 0, "Win for player1"},
-                { 0, 4, "Win for player2"},
+                { 1, 0,  "player1", "player2",  "Fifteen-Love"},
+                { 0, 1,  "player1", "player2",  "Love-Fifteen"},
+                { 2, 0,  "player1", "player2",  "Thirty-Love"},
+                { 0, 2,  "player1", "player2",  "Love-Thirty"},
+                { 3, 0,  "player1", "player2",  "Forty-Love"},
+                { 0, 3,  "player1", "player2",  "Love-Forty"},
+                { 4, 0,  "player1", "player2",  "Win for player1"},
+                { 0, 4,  "player1", "player2",  "Win for player2"},
                 
-                { 2, 1, "Thirty-Fifteen"},
-                { 1, 2, "Fifteen-Thirty"},
-                { 3, 1, "Forty-Fifteen"},
-                { 1, 3, "Fifteen-Forty"},
-                { 4, 1, "Win for player1"},
-                { 1, 4, "Win for player2"},
+                { 2, 1,  "player1", "player2",  "Thirty-Fifteen"},
+                { 1, 2,  "player1", "player2",  "Fifteen-Thirty"},
+                { 3, 1,  "player1", "player2",  "Forty-Fifteen"},
+                { 1, 3,  "player1", "player2",  "Fifteen-Forty"},
+                { 4, 1,  "player1", "player2",  "Win for player1"},
+                { 1, 4,  "player1", "player2",  "Win for player2"},
 
-                { 3, 2, "Forty-Thirty"},
-                { 2, 3, "Thirty-Forty"},
-                { 4, 2, "Win for player1"},
-                { 2, 4, "Win for player2"},
+                { 3, 2,  "player1", "player2",  "Forty-Thirty"},
+                { 2, 3,  "player1", "player2",  "Thirty-Forty"},
+                { 4, 2,  "player1", "player2",  "Win for player1"},
+                { 2, 4,  "player1", "player2",  "Win for player2"},
                 
-                { 4, 3, "Advantage player1"},
-                { 3, 4, "Advantage player2"},
-                { 5, 4, "Advantage player1"},
-                { 4, 5, "Advantage player2"},
-                { 15, 14, "Advantage player1"},
-                { 14, 15, "Advantage player2"},
+                { 4, 3,  "player1", "player2",  "Advantage player1"},
+                { 3, 4,  "player1", "player2",  "Advantage player2"},
+                { 5, 4,  "player1", "player2",  "Advantage player1"},
+                { 4, 5,  "player1", "player2",  "Advantage player2"},
+                { 15, 14,  "player1", "player2",  "Advantage player1"},
+                { 14, 15,  "player1", "player2",  "Advantage player2"},
 
-                { 6, 4, "Win for player1"},
-                { 4, 6, "Win for player2"},
-                { 16, 14, "Win for player1"},
-                { 14, 16, "Win for player2"},
+                { 6, 4,  "player1", "player2",  "Win for player1"},
+                { 4, 6,  "player1", "player2",  "Win for player2"},
+                { 16, 14,  "player1", "player2",  "Win for player1"},
+                { 14, 16,  "player1", "player2",  "Win for player2"},
         });
     }
 
@@ -85,6 +89,12 @@ public class TennisTest {
     @Test
     public void checkAllScoresTennisGame2() {
         TennisGame2 game = new TennisGame2("player1", "player2");
+        checkAllScores(game);
+    }
+
+    @Test
+    public void checkAllScoresTennisGame2WithDifferentNames() {
+        TennisGame2 game = new TennisGame2("PlayerA", "PlayerB");
         checkAllScores(game);
     }
 
