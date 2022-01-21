e380c998ec90758903c1f90186eefc3db5f55d19
Simon <simonbesteiro@gmail.com>
2022-01-21 10:32:55 +0100

    refactor wip

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -21,15 +21,17 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore = 0;
+
         if (m_score1 == m_score2) {
             score = getScoreWhenEqual();
         } else if (m_score1 >= 4 || m_score2 >= 4) {
+
             int minusResult = m_score1 - m_score2;
             if (minusResult == 1) score = "Advantage player1";
             else if (minusResult == -1) score = "Advantage player2";
             else if (minusResult >= 2) score = "Win for player1";
             else score = "Win for player2";
+
         } else {
             score = getDefaultScore(score, m_score1) + "-" + getDefaultScore(score, m_score2);
         }
